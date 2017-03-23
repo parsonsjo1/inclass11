8a33b3fe6efdc2621383145a656df0eb4d5459bc
parsonsjo1 <jopa.d.parsons@gmail.com>
2017-03-23 14:12:22 -0600

    finished in class exercise

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,31 +1,59 @@
-angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
+var app = angular.module('commentsApp',[]);
+
+app.controller('MainController', [
+  '$scope', '$http',
+  function($scope, $http) {
+
     $scope.comments = [];
+
     $scope.addComment = function() {
-      var newcomment = {title:$scope.formContent,upvotes:0};
-      $scope.formContent='';
-      $http.post('/comments', newcomment).success(function(data){
-        $scope.comments.push(data);
+      console.log($scope.formContent);
+      if($scope.formContent == undefined) {
+        return;
+      }
+
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0
       });
+
+      $scope.formContent = '';
+
     };
-    $scope.upvote = function(comment) {
-      return $http.put('/comments/' + comment._id + '/upvote')
-        .success(function(data){
-          console.log("upvote worked");
-          comment.upvotes = data.upvotes;
-        });
+
+    $scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
     };
-	$scope.incrementUpvotes = function(comment) {
-	  $scope.upvote(comment);
-    };
+
     $scope.getAll = function() {
-      return $http.get('/comments').success(function(data){
+      return $http.get('/comments').success(function(data) {
         angular.copy(data, $scope.comments);
       });
     };
+
+    $scope.create = function(comment) {
+      return $http.post('/comments', comment).success(function(data) {
+        $scope.comments.push(data);
+      });
+    };
+
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+      .success(function(data){
+        comment.upvotes += 1;
+      })
+    };
+
+    $scope.delete = function(comment) {
+      $http.delete('/comments/' + comment._id)
+      .success(function(data) {
+
+      });
+      $scope.getAll();
+    }
+    
     $scope.getAll();
 
   }
 ]);
+
--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,55 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Comment = mongoose.model('Comment');
 
-router.get('/comments', function(req, res, next) {
-  Comment.find(function(err, comments){
-    if(err){ return next(err); }
+router.get("/comments", function(req, res, next) {
+  Comment.find(function(err, comments) {
+    if(err) {
+      return next(err);
+    }
     res.json(comments);
   });
 });
 
-router.post('/comments', function(req, res, next) {
+router.get("comments/:comment", function(req, res) {
+  res.json(req.comment);
+});
+
+router.post("/comments", function(req,res,next) {
   var comment = new Comment(req.body);
-  comment.save(function(err, comment){
-    if(err){ return next(err); }
+  comment.save(function(err, comment) {
+    if(err) {
+      return next(err);
+    }
     res.json(comment);
   });
 });
 
-router.param('comment', function(req, res, next, id) {
+router.put('/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) {
+      return next(err);
+    }
+    res.json(comment);
+  });
+});
+
+router.delete('/comments/:comment', function(req,res) {
+  req.comment.remove();
+  res.json(req.comment);
+});
+
+router.param('comment', function(req,res,next,id) {
   var query = Comment.findById(id);
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!comment) { return next(new Error("can't find comment")); }
+  query.exec(function(err,comment) {
+    if(err){
+      return next(err);
+    }
+    if(!comment) {
+      return next(new Error("can't find comment"));
+    }
     req.comment = comment;
     return next();
   });
 });
 
-router.get('/comments/:comment', function(req, res) {
-  res.json(req.comment);
-});
-
-router.put('/comments/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, comment){
-    if (err) { return next(err); }
-    res.json(comment);
-  });
-});
 module.exports = router;
